0cd94136b360830242ff0d4d95e78fe45836970c
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-08-26 18:18:56 -0300

    feat: estilos y modificación de la sección de servicios

--- src/components/Servicios/Tecnico.tsx
+++ src/components/Servicios/Tecnico.tsx
@@ -1,29 +1,38 @@
 import React from "react";
 import { BsWhatsapp } from "react-icons/bs";
-
 import ButtonSolicitar from "../UI/ButtonSolicitar";
 
 const Tecnico: React.FC = () => {
   return (
-    <div className="flex md:flex-row flex-col-reverse justify-center items-center gap-20 px-4 md:px-8">
-      {/*TODO Columna izquierda: imagen */}
-      <div className="flex-shrink-0">
+    <div className="flex md:flex-row flex-col justify-center items-center gap-12 px-4 md:px-8">
+      {/*TODO Columna izquierda */}
+      <div className="relative flex-shrink-0">
+        <div className="right-28 bottom-54 -z-10 absolute border-9 border-cta w-28 md:w-40 h-[calc(100%-12rem)] -translate-x-16"></div>
+
         <img
-          src="/tecnico.jpg"
-          alt="Técnico"
-          className="shadow-md rounded-lg w-full max-w-sm"
+          src="/tecnico.webp"
+          alt="Tecnico"
+          className="shadow-md rounded-lg w-72 h-auto"
+        />
+
+        <img
+          src="/tecnico2.webp"
+          alt="Aire acondicionado"
+          className="bottom-9 -left-10 absolute shadow-lg border-[#2F456F] border-6 rounded-r-full rounded-l-full w-36 h-24"
         />
       </div>
 
-      {/*TODO Columna derecha: texto, marquee y botón */}
-      <div className="flex flex-col gap-6 max-w-md md:text-left text-center">
-        <h2 className="font-bold text-white text-2xl">
+      {/*TODO Columna derecha */}
+      <div className="flex flex-col gap-10 max-w-md md:text-left text-center">
+        <h2 className="font-bold text-white text-2xl md:text-4xl">
           Técnico Aire Acondicionado
         </h2>
 
-        <p className="text-gray-300">
-          Nos encargamos de cada instalación con la máxima seguridad y calidad,
-          para que tu equipo funcione a la perfección y sin preocupaciones.
+        <p className="text-gray-300 text-sm">
+          Nos encargamos de cada instalación eléctrica con la máxima seguridad y
+          calidad, para que disfrutes de tu hogar o negocio sin preocupaciones.
+          Nos encargamos de cada instalación eléctrica con la máxima seguridad y
+          calidad, para que disfrutes de tu hogar o negocio sin preocupaciones.
         </p>
 
         <div className="flex justify-center md:justify-start gap-4 mt-4">
