0636ddd5129cb8b9f0ca9412f142e6e1b5da8652
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-08-28 14:53:07 -0300

    feat: agregar botón flotante de WhatsApp para todas las páginas y secciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import WhatsAppButton from "./components/Globo/WhatsAppButton";
 import RoutesProvider from "./RoutesProvider";
 
 function App() {
   return (
     <div className="bg-background">
       <RoutesProvider />
+      <WhatsAppButton
+        whatsappNumber="+541154781055"
+        message="Hola, quiero información"
+      />
     </div>
   );
 }
